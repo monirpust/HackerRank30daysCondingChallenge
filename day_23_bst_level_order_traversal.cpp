1684959b1fb65cdbd5d9c3f635a59f4e7b1f7f90
monirpust <mdmonir1997@gmal.com>
2022-11-13 16:41:22 +0600

    day 23 bst level order traversal dfs done

---
+++ day_23_bst_level_order_traversal.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;	
+class Node{
+    public:
+        int data;
+        Node *left,*right;
+        Node(int d){
+            data=d;
+            left=right=NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data){
+            if(root==NULL){
+                return new Node(data);
+            }
+            else{
+                Node* cur;
+                if(data<=root->data){
+                    cur=insert(root->left,data);
+                    root->left=cur;
+                }
+                else{
+                   cur=insert(root->right,data);
+                   root->right=cur;
+                 }           
+           return root;
+           }
+        }
+
+	void levelOrder(Node * root){
+      //Write your code here
+        Node* current;
+        queue<Node*>Q;
+        if(root == NULL) return ;
+        
+        Q.push(root);
+        
+        while(!Q.empty())
+        {
+        	cout<<"inside while loop of Q"<<endl;
+            current = Q.front();
+            if(current->left != NULL)
+            	Q.push(current->left);
+            if(current->right != NULL)
+            	Q.push(current->right);
+            
+            cout<<current->data<<" ";
+            
+            Q.pop();
+        }
+        
+	}
+
+};//End of Solution
+int main(){
+    Solution myTree;
+    Node* root=NULL;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        root= myTree.insert(root,data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
